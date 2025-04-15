456a27ef8077b88f02862ce00d72dfb1f69b02cc
Seçkin <seckiinyener@gmail.com>
2025-04-15 22:00:25 +0300

    initial implementation for layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,80 @@
-import { html, LitElement } from "lit";
+import { css, html, LitElement } from "lit";
+import router from '../router.js';
+import { Router } from "@vaadin/router";
 
 export class Layout extends LitElement {
+
+    static properties = {
+        currentRoute: { state: true }
+    };
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.currentRoute = router.location.pathname;
+        window.addEventListener("vaadin-router-location-changed", this.handleRouteChange);
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        window.removeEventListener("vaadin-router-location-changed", this.handleRouteChange);
+    }
+
+    handleRouteChange = (e) => {
+        const event = e;
+        this.currentRoute = event.detail.location.pathname;
+    };
+
+    navigate = (path) => {
+        Router.go(path)
+    }
+
     render() {
         return html`
-            <div>Hebele</div>
+        <nav>
+            <div class="${this.currentRoute === '/' ? 'active' : ''}" @click=${() => this.navigate("/")}>Employees</div>
+            <div class="${this.currentRoute === '/create' ? 'active' : ''}" @click=${() => this.navigate("/create")}>Create Employee</div>
+        </nav>
+        <div class="child-routes">
             <slot></slot>
+        </div>
         `
     }
+
+    static styles = css`
+
+        .material-icons {
+            font-family: 'Material Icons';
+            font-size: 24px;
+            vertical-align: middle;
+        }
+
+        .navigation-menu {
+            background-color: white;
+        }
+
+        .child-routes {
+            padding: 1rem 2rem;
+        }
+
+        nav {
+            background: #2c3e50;
+            color: white;
+            padding: 1rem;
+            display: flex;
+            gap: 1rem;
+        }
+
+        nav div {
+            cursor: pointer;
+            padding: 0.5rem 1rem;
+            border-radius: 4px;
+        }
+
+        nav div.active {
+            background-color: #1abc9c;
+            font-weight: bold;
+        }
+    `
 }
 
 customElements.define("layout-component", Layout);
